656e178a07d9d8e697c207895e96777c23bd82bb
murodjon7777 <88221222+murodjon7777@users.noreply.github.com>
2022-12-10 19:16:21 +0500

    Update __init__.py
    kril noo

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,7 +2,7 @@ from . import help
 from . import start
 from . import valyuta
 from . import translate
-from . import kl
+# from . import kl
 from . import update_db
 from . import admin
 from . import echo
